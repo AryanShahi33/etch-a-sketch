7a0ec22428d0e3c72dd7fdb021a378c885b69773
Aryan <209350547+AryanShahi33@users.noreply.github.com>
2026-06-26 19:15:16 +0545

    Make grid

---
+++ script.js
@@ -0,0 +1,6 @@
+let container = document.getElementById("container")
+for (let i = 0;i<256;i++){
+    let grid = document.createElement("div")
+    grid.classList.add("grid")
+    container.appendChild(grid)
+}
