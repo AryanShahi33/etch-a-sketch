3e06ff228aa28ea09ea86ea0b1ca47eea1c2aed4
Aryan <209350547+AryanShahi33@users.noreply.github.com>
2026-06-26 21:06:01 +0545

    Add Hover to paint

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ for (let i = 0;i<16;i++){
     for (let j = 0;j<16;j++){
         let grid = document.createElement("div")
         grid.classList.add("grid")
+        grid.addEventListener('mouseover',(event) =>{
+        grid.style.backgroundColor = "red";
+    });
         container.appendChild(grid)
     }
 }
+
