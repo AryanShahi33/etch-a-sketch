13a2315c4acc962026c597042ef307e018c36323
Aryan <209350547+AryanShahi33@users.noreply.github.com>
2026-06-30 16:20:53 +0545

    Automatically change div square size according to grid size

--- script.js
+++ script.js
@@ -9,14 +9,15 @@ button.addEventListener('click',() => {
 
 
 function generateGrid(Size){
-
+    let gridSize = Size*Size;
+    let gridBoxSize = 1000*1000/gridSize;
+    let gridDimension = Math.sqrt(gridBoxSize)-2 + "px" ;
 for (let i = 0;i<Size*Size;i++){
         let grid =  document.createElement("div");
+        grid.style.width = gridDimension;
+        grid.style.height = gridDimension;
         grid.classList.add("Grid");
         gridBox.appendChild(grid);
     }
-    let gridSize = Size*Size;
-    let gridBoxSize = 1000*1000/gridSize;
-    let gridDimension = Math.sqrt(gridBoxSize) + "px";
-    console.log(gridDimension);
+
 }
